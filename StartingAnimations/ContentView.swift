ae9b680586745af27ba4ac18714c5dbc948cff80
Stefan Walkner <stefan@arkulpa.at>
2021-05-06 19:49:56 +0200

    Initial Commit

---
+++ StartingAnimations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StartingAnimations
+//
+//  Created by Stefan Walkner on 06.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StartingAnimations/StartingAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartingAnimationsApp.swift
+//  StartingAnimations
+//
+//  Created by Stefan Walkner on 06.05.21.
+//
+
+import SwiftUI
+
+@main
+struct StartingAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
